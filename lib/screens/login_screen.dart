4baf10b806a94ab417a80a8c1ece5fabcfae3f0e
zinon software <business.zinon@gmail.com>
2023-02-07 20:31:18 +0200

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Instagram',
+            style: TextStyle(fontSize: 50.0,),),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
